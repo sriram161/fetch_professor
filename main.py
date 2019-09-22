62e3fa3628eff445a847710c5bdc5e1e0cd44542
sriram161 <k.sriramsagar@gmail.com>
2019-09-22 14:47:10 -0400

    Code commet draft

--- main.py
+++ main.py
@@ -2,9 +2,9 @@
 """
 Created on Sat Jan 13 15:42:35 2018
 
-@author: ksrir
+@author: Sri Ram Sagar k
 
-@Description: This is the main file to use package getmails.
+@Description: fetche e-mail address for a given url in URL_K_FACULTY.
 """
 
 from getmails.html_fetcher import URLClerk
@@ -13,36 +13,32 @@ from getmails.constants import ReObjects
 from urllib.error import HTTPError
 from itertools import chain
 
-URL_KRASNOW_FACULTY = "https://krasnow.gmu.edu/kifaculty/"
+URL_K_FACULTY = "https://xxxxxxx.xxx.xxx/kifaculty/" # Input URL to fetch email addresses.
 
-clerk = URLClerk()
+if __name__ == "__main__":
+    clerk = URLClerk()
+    html_raw = clerk.get_html(link = URL_K_FACULTY)
 
-html_raw = clerk.get_html(link = URL_KRASNOW_FACULTY)
-
-analyzer = HtmlClerk(html_raw)
-lines = analyzer.get_tag('ul')
-prof_links = {a.get_text(): a.get('href') for li in lines[-2].find_all('li') for a in li.find_all('a') }
-temp_df = []
-for prof,link in prof_links.items():
-    try:
-        frame = []
-        obj = HtmlClerk(clerk.get_html(link))
-        emails = [obj(text=re_obj) for re_obj in ReObjects.email_patterns]
-        phones = [obj(text=re_obj) for re_obj in ReObjects.phone_patterns]
-        frame.append(prof)
-        frame.extend(list(chain.from_iterable(emails)))
-        frame.extend(list(chain.from_iterable(phones)))
-        temp_df.append(frame)
+    analyzer = HtmlClerk(html_raw)
+    lines = analyzer.get_tag('ul')
+    prof_links = {a.get_text(): a.get('href') for li in lines[-2].find_all('li') for a in li.find_all('a') }
+    temp_df = []
+    for prof,link in prof_links.items():
+        try:
+            frame = []
+            obj = HtmlClerk(clerk.get_html(link))
+            emails = [obj(text=re_obj) for re_obj in ReObjects.email_patterns]
+            phones = [obj(text=re_obj) for re_obj in ReObjects.phone_patterns]
+            frame.append(prof)
+            frame.extend(list(chain.from_iterable(emails)))
+            frame.extend(list(chain.from_iterable(phones)))
+            temp_df.append(frame)
+        except HTTPError:
+            continue
         
-    except HTTPError:
-        continue
     
-  
-# Convert data to pandas data frame and Write to an excel file using pandas
-import pandas as pd
-df = pd.DataFrame(temp_df)
-print(df)
-df.to_excel('krasnow_faculty.xls')
-
-        
-        
+    # Convert data to pandas data frame and Write to an excel file using pandas
+    import pandas as pd
+    df = pd.DataFrame(temp_df)
+    print(df)
+    df.to_excel('k_faculty.xls')
